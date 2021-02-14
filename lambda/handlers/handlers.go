0ab5bf3986fffa6ca1b7012a80d361ebd998a84a
biosbuddha <biosbuddha@zen.lan>
2021-02-14 17:41:04 -0600

    adding vt hash scan

--- lambda/handlers/handlers.go
+++ lambda/handlers/handlers.go
@@ -1,27 +1,51 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
-	"net/http"
+	"log"
 
-	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 
 	"github.com/likexian/whois-go"
 	whoisparser "github.com/likexian/whois-parser-go"
+
+	vt "github.com/VirusTotal/vt-go"
 )
 
-func WhoisHandler(ctx context.Context, req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	domain := req.QueryStringParameters["domain"]
+//WhoisHandler is a wrapper for th whois website via a golang package
+//it takes the aws gateway request and returns a response
+func WhoisHandler(domain string) (string, error) {
 
 	result, _ := whois.Whois(domain)
 	parsResult, _ := whoisparser.Parse(result)
-
-	resp := events.APIGatewayProxyResponse{}
 	stringBody, _ := json.Marshal(parsResult)
-	resp.Body = string(stringBody)
-	resp.StatusCode = http.StatusOK
-	resp.IsBase64Encoded = false
-	resp.Headers = map[string]string{"Content-Type": "application/json"}
-	return &resp, nil
+
+	return string(stringBody), nil
+}
+
+//VirusTotalHandler is a wrapper for th whois website via a golang package
+//it takes the aws gateway request and returns a response
+func VirusTotalHandler(filehash string) (string, error) {
+
+	apikey := getAWSSecret
+	client := vt.NewClient(apikey())
+	file, err := client.GetObject(vt.URL("files/%s", filehash))
+	if err != nil {
+		log.Fatal(err)
+	}
+	stringBody, _ := json.Marshal(file)
+	return string(stringBody), nil
+
+}
+
+func getAWSSecret() string {
+	svc := secretsmanager.New(session.New())
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String("virustotalapi"),
+	}
+	result, _ := svc.GetSecretValue(input)
+	return *result.SecretString
+
 }
--- lambda/main.go
+++ lambda/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"context"
 	"lambdaapi/handlers"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
+func router(ctx context.Context, req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lookuptype := req.QueryStringParameters["lookuptype"]
 
-	lambda.Start(handlers.WhoisHandler)
+	var bodystring string
+	if lookuptype == "whois" {
+		domain := req.QueryStringParameters["domain"]
+		s, _ := handlers.WhoisHandler(domain)
+		bodystring = s
+	}
+	if lookuptype == "vt" {
+		filehash := req.QueryStringParameters["filehash"]
+		s, _ := handlers.VirusTotalHandler(filehash)
+		bodystring = s
+	}
+	resp := events.APIGatewayProxyResponse{}
+	resp.Body = string(bodystring)
+	resp.StatusCode = http.StatusOK
+	resp.IsBase64Encoded = false
+	resp.Headers = map[string]string{"Content-Type": "application/json"}
+	return &resp, nil
+}
+
+func main() {
+	lambda.Start(router)
 }
